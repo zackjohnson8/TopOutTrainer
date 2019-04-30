bb157025d6a985a67ee16c1aaca81653df098ce4
Zachary Johnson <zackjohnson8@gmail.com>
2019-04-30 16:03:39 -0700

    Added: setting button navigates to new selection window. Added graph button for future use.

---
+++ TopOutTrainer/TopOutTrainer/StaticFiles/TimerPageUISettings.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace TopOutTrainer.StaticFiles
+{
+    public static class TimerPageUISettings
+    {
+        // Default values if not set to anything yet.
+        private static int reps = 3;
+        private static int sets = 3;
+        private static int setsRestTime = 180; // seconds
+        private static int repsRestTime = 90; // seconds
+
+
+
+        public static int Reps
+        {
+            get
+            {
+                // TODO(zack): check later for saved values to use.
+                return reps;
+            }
+            private set
+            {
+                reps = value;
+            }
+        }
+
+        public static int Sets
+        {
+            get
+            {
+                return sets;
+            }
+            private set
+            {
+                sets = value;
+            }
+        }
+
+        public static int SetsRestTime
+        {
+            get
+            {
+                return setsRestTime;
+            }
+            private set
+            {
+                setsRestTime = value;
+            }
+        }
+
+        public static int RepsRestTime
+        {
+            get
+            {
+                return repsRestTime;
+            }
+            private set
+            {
+                repsRestTime = value;
+            }
+        }
+    }
+}
