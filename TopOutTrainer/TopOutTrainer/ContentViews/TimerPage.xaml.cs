c908310fd70cbb26163c6102c09b67a4fa4fe9e0
Zachary Johnson <zackjohnson8@gmail.com>
2019-04-30 01:01:37 -0700

    Clean: Removed previous project files and cleaned up design a bit.

--- TopOutTrainer/TopOutTrainer/ContentViews/TimerPage.xaml.cs
+++ TopOutTrainer/TopOutTrainer/ContentViews/TimerPage.xaml.cs
@@ -10,7 +10,7 @@ using TopOutTrainer.CustomOption;
 namespace TopOutTrainer.ContentViews
 {
 	//[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class TimerPage_ContentPage : ContentPage
+	public partial class TimerPage : ContentPage
 	{
         private TimerOption myTimerOption;
 
@@ -40,7 +40,7 @@ namespace TopOutTrainer.ContentViews
         private double screenWidth;
         private double screenHeight;
 
-        public TimerPage_ContentPage()
+        public TimerPage()
         {
 
             NavigationPage.SetHasNavigationBar(this, false);
@@ -315,36 +315,9 @@ namespace TopOutTrainer.ContentViews
             };
         }
 
-        public void Create_BitMap()
-        {
-
-
-            //// Bitmap Size
-            //double rows = screenHeight;
-            //double cols = screenHeight;
-            //bmpMaker = new BmpMaker((int)rows, (int)cols);
-
-
-            //// Convert hang & rest into seconds then draw images to the bitmap
-            //int hangSeconds = 10;
-            //int restSeconds = 10;
-            //bmpMaker.DrawCircle((int)rows, (int)cols, 30, hangSeconds, restSeconds, 3); // TODO remove hardcode
-            //ImageSource imageSource = bmpMaker.Generate();
-
-            //bitmapI = new Image
-            //{
-            //    HorizontalOptions = LayoutOptions.CenterAndExpand,
-            //    VerticalOptions = LayoutOptions.CenterAndExpand,
-            //    Aspect = Aspect.Fill,
-            //    Source = imageSource
-            //};
-
-            //bitmapContainer.Children.Add(bitmapI);
-        }
-
         private void OptionButtonClicked(object sender, EventArgs args)
         {
-            //MainPage = new NavigationPage(new TopOutTrainer.ContentViews.TimerPage_ContentPage());
+            //MainPage = new NavigationPage(new TopOutTrainer.ContentViews.TimerPage());
             //MainPage.SetValue(NavigationPage.BarBackgroundColorProperty, Color.FromHex(bannerBackgroundColor));
         }
 
@@ -394,4 +367,275 @@ namespace TopOutTrainer.ContentViews
 //{
 //    //valueLabel.Text = args.NewValue.ToString("F3");
 //    //valueLabel.Text = ((Slider)sender).Value.ToString("F3");
+//}
+
+
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Text;
+//using Xamarin.Forms;
+
+//namespace TopOutTrainer
+//{
+//    public class ContentViewHandler_TimerPage
+//    {
+
+//        public StackLayout MainContainer;
+//        public Label labelTimerText;
+//        public Image bitmapImage;
+//        public StackLayout stackLayoutButtonHolder;
+//        public StackLayout stackLayoutBitmapHolder;
+//        public Button buttonStart;
+//        public Button buttonStop;
+//        public Button buttonReset;
+//        public View Content;
+//        public String totalTime = "00:00";
+
+//        private int myHangTime_Min;
+//        private int myHangTime_Sec;
+//        private int myRestTime_Min;
+//        private int myRestTime_Sec;
+//        private int myHangInterval_Count;
+
+//        // Circle Graphic Thickness
+//        private const int GraphicThickness = 100;
+
+//        public Color TextColor
+//        {
+//            private set;
+//            get;
+//        } = Color.FromHex("#ffffff");
+
+//        public Color BackgroundColor
+//        {
+//            private set;
+//            get;
+//        } = Color.FromHex("#303030");
+
+//        private void Create_TPMainContainer()
+//        {
+
+//            MainContainer = new StackLayout
+//            {
+//                VerticalOptions = LayoutOptions.FillAndExpand,
+//                Margin = new Thickness(10),
+//            };
+
+//        }
+
+//        private void Create_TPTotalTimeLabel()
+//        {
+
+//            // Timer Text
+//            // Use previous slection screen to determine text number
+//            labelTimerText = new Label
+//            {
+//                Text = totalTime,
+//                FontSize = 64,
+//                FontAttributes = FontAttributes.Bold,
+//                //HorizontalOptions = LayoutOptions.CenterAndExpand,
+//                //VerticalOptions = LayoutOptions.CenterAndExpand,
+//                HorizontalTextAlignment = TextAlignment.Center,
+//                HeightRequest = 120,
+//                BackgroundColor = BackgroundColor,
+//                TextColor = TextColor,
+
+//            };
+
+
+//            //Font-Family
+//            if (Device.RuntimePlatform == Device.iOS)
+//            {
+//                labelTimerText.FontFamily = "OpenSans-Regular";
+//            }
+//            if (Device.RuntimePlatform == Device.Android)
+//            {
+//                labelTimerText.FontFamily = "font/montserrat/MontserratAlternates-Bold.otf#MontserratAlternates-Bold"; // Figure out where this is looking to find the string
+//            }
+
+
+//            MainContainer.Children.Add(labelTimerText);
+
+//        }
+
+//        private void Create_TPBitMap()
+//        {
+
+//            // Bitmap stack container
+//            stackLayoutBitmapHolder = new StackLayout
+//            {
+//                VerticalOptions = LayoutOptions.Center,
+//                HorizontalOptions = LayoutOptions.FillAndExpand,
+//                Margin = new Thickness(10),
+//                BackgroundColor = BackgroundColor,
+//                Orientation = StackOrientation.Horizontal,
+//            };
+
+//            // Bitmap Size
+//            int rows = 800;
+//            int cols = 800;
+//            BmpMaker bmpMaker = new BmpMaker(cols, rows);
+
+
+//            // Convert hang & rest into seconds then draw images to the bitmap
+//            int hangSeconds = (myHangTime_Min * 60) + myHangTime_Sec;
+//            int restSeconds = (myRestTime_Min * 60) + myRestTime_Sec;
+//            bmpMaker.DrawCircle(rows, cols, GraphicThickness, hangSeconds, restSeconds, myHangInterval_Count); // TODO remove hardcode
+
+//            ImageSource imageSource = bmpMaker.Generate();
+
+//            bitmapImage = new Image
+//            {
+//                HorizontalOptions = LayoutOptions.CenterAndExpand,
+//                VerticalOptions = LayoutOptions.CenterAndExpand,
+//                Aspect = Aspect.Fill,
+//                Source = imageSource
+//            };
+
+//            stackLayoutBitmapHolder.Children.Add(bitmapImage);
+//            MainContainer.Children.Add(stackLayoutBitmapHolder);
+//        }
+
+//        private void Create_TPStartStopButton()
+//        {
+//            // START BUTTON //
+//            buttonStart = new Button
+//            {
+//                Text = "Start",
+//                VerticalOptions = LayoutOptions.CenterAndExpand,
+//                HorizontalOptions = LayoutOptions.CenterAndExpand,
+//                WidthRequest = 120,
+//                HeightRequest = 40,
+//                BorderColor = Color.FromHex("#66ffff"),
+//                BorderWidth = 3,
+//                CornerRadius = 2,
+//            };
+//            buttonStart.Clicked += StartButtonClicked;
+
+
+//            buttonStop = new Button
+//            {
+//                Text = "Stop",
+//                VerticalOptions = LayoutOptions.CenterAndExpand,
+//                HorizontalOptions = LayoutOptions.CenterAndExpand,
+//                WidthRequest = 120,
+//                HeightRequest = 40,
+//                BorderColor = Color.FromHex("#66ffff"),
+//                BorderWidth = 3,
+//                CornerRadius = 2,
+//            };
+//            buttonStop.Clicked += StopButtonClicked;
+
+//            buttonReset = new Button
+//            {
+//                Text = "Reset",
+//                VerticalOptions = LayoutOptions.CenterAndExpand,
+//                HorizontalOptions = LayoutOptions.CenterAndExpand,
+//                WidthRequest = 120,
+//                HeightRequest = 40,
+//                BorderColor = Color.FromHex("#66ffff"),
+//                BorderWidth = 3,
+//                CornerRadius = 2,
+//                IsVisible = false
+//            };
+//            buttonReset.Clicked += ResetButtonClicked;
+
+//            // Button Holder Creation and fill with new buttons
+//            stackLayoutButtonHolder = new StackLayout
+//            {
+//                VerticalOptions = LayoutOptions.FillAndExpand,
+//                HorizontalOptions = LayoutOptions.FillAndExpand,
+//                Margin = new Thickness(10),
+//                BackgroundColor = Color.FromHex("#303030"),
+//                Orientation = StackOrientation.Horizontal,
+//            };
+
+//            stackLayoutButtonHolder.Children.Add(buttonStart);
+//            stackLayoutButtonHolder.Children.Add(buttonStop);
+//            stackLayoutButtonHolder.Children.Add(buttonReset);
+//            MainContainer.Children.Add(stackLayoutButtonHolder);
+//        }
+
+//        // CONSTRUCTOR
+//        public ContentViewHandler_TimerPage(int hangTime_min, int hangTime_sec, int restTime_min, int restTime_sec, int hangInterval_count)
+//        {
+
+//            // Take the passed in values and build the image using these
+//            myHangTime_Min = hangTime_min;
+//            myHangTime_Sec = hangTime_sec;
+//            myRestTime_Min = restTime_min;
+//            myRestTime_Sec = restTime_sec;
+//            myHangInterval_Count = hangInterval_count;
+
+//        }
+
+//        public View GetContentView()
+//        {
+
+//            // Create main container for objects
+//            Create_TPMainContainer();
+
+//            // Create upper label for total time
+//            Create_TPTotalTimeLabel();
+
+//            // Create bitmap for timer animation
+//            Create_TPBitMap();
+
+//            // Create button holder and buttons
+//            Create_TPStartStopButton();
+
+//            return Content = MainContainer;
+
+//        }
+
+//        // EVENTS
+//        private void StartButtonClicked(object sender, EventArgs args)
+//        {
+//            StopWatch.Start();
+//            buttonReset.IsVisible = false;
+//            buttonStop.IsVisible = true;
+//        }
+
+//        private void StopButtonClicked(object sender, EventArgs args)
+//        {
+//            StopWatch.Stop();
+//            buttonReset.IsVisible = true;
+//            buttonStop.IsVisible = false;
+//        }
+
+//        private void ResetButtonClicked(object sender, EventArgs args)
+//        {
+//            StopWatch.Reset();
+//        }
+
+//    }
+
+
+//}
+
+//public void Create_BitMap()
+//{
+
+
+//// Bitmap Size
+//double rows = screenHeight;
+//double cols = screenHeight;
+//bmpMaker = new BmpMaker((int)rows, (int)cols);
+
+
+//// Convert hang & rest into seconds then draw images to the bitmap
+//int hangSeconds = 10;
+//int restSeconds = 10;
+//bmpMaker.DrawCircle((int)rows, (int)cols, 30, hangSeconds, restSeconds, 3); // TODO remove hardcode
+//ImageSource imageSource = bmpMaker.Generate();
+
+//bitmapI = new Image
+//{
+//    HorizontalOptions = LayoutOptions.CenterAndExpand,
+//    VerticalOptions = LayoutOptions.CenterAndExpand,
+//    Aspect = Aspect.Fill,
+//    Source = imageSource
+//};
+
+//bitmapContainer.Children.Add(bitmapI);
 //}
---
+++ TopOutTrainer/TopOutTrainer/ContentViews/TimerPageSettings.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TopOutTrainer.ContentViews
+{
+	public partial class TimerPageSettings : ContentPage
+	{
+ 
+    }
+}
