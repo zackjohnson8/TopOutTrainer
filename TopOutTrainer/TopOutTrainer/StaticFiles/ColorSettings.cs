aaba4b1d45e8f865570b66210ec8c97a6bbc6d96
Zachary Johnson <zackjohnson8@gmail.com>
2019-06-04 15:34:24 -0700

    Added: bitmap now shows with countdown. Needs to still change colors and values need to be sent throughout timer.

---
+++ TopOutTrainer/TopOutTrainer/StaticFiles/ColorSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Xamarin.Forms;
+namespace TopOutTrainer.StaticFiles
+{
+    public static class ColorSettings
+    {
+        public static Color getReadyColor { get; set; } = Color.FromHex("#FFFF00");
+        public static Color mainGrayColor { get; set; } = Color.FromHex("#303030");
+
+    }
+}
