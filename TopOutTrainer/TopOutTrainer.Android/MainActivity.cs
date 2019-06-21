9e17cd86e33d41e6cd3f4dc137dae8a5e8fb2af3
Zachary Johnson <zackjohnson8@gmail.com>
2019-06-20 20:19:19 -0700

    Fixed: Locked screen orientation to portrait mode

--- TopOutTrainer/TopOutTrainer.Android/MainActivity.cs
+++ TopOutTrainer/TopOutTrainer.Android/MainActivity.cs
@@ -6,10 +6,11 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms;
 
 namespace TopOutTrainer.Droid
 {
-    [Activity(Label = "TopOutTrainer", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "TopOutTrainer", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
